63dff60eba54d581f6ec2083aed4c5c131447f87
Sandeep Patel <sandeeppatel@sandeep.local>
2024-07-25 10:44:11 +0530

    sandeep

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,20 +1,23 @@
-import { IconButton } from "@chakra-ui/react";
+import { IconButton, background } from "@chakra-ui/react";
 import React from "react";
 import { AddIcon } from "@chakra-ui/icons";
+import { color } from "framer-motion";
 
 const floatingButtonStyle = {
     position: "fixed",
-    top: "5rem",
+    bottom: "5rem",
     right: "5rem",
     zIndex: 5000,
+    
 };
 
 const AddButton = ({ onOpen }) => {
     return ( 
       <div style={floatingButtonStyle}>
       <IconButton
+      color={"red"}
+      backgroundColor={"black"}
         size="lg"
-        
         onClick={onOpen}
         colorScheme="teal"
         icon={<AddIcon />}
